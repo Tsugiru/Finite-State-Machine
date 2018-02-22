f92e54c55be4403b9bc83d35a73536edfc13b3dc
Elie Maamary <eliemaamary17@gmail.com>
2018-02-22 17:56:19 +0200

    Update port.h

--- include/port.h
+++ include/port.h
@@ -3,12 +3,12 @@
 
 class Port {
 public:
-    bool active;
-    Port() { active = true; }
-    Port(bool initial_status) { active = initial_status; }
-    void activate() { active = true; }
-    void deactivate() { active = false; }
-    void toggle() { active = !active; }
+	bool active;
+	Port() { active = true; }
+	Port(bool initial_status) { active = initial_status; }
+	void activate() { active = true; }
+	void deactivate() { active = false; }
+	void toggle() { active = !active; }
 
 };
 
