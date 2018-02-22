59fbd781726549c33bc426166383916a2b84c5e1
Elie Maamary <eliemaamary17@gmail.com>
2018-02-22 17:46:52 +0200

    bug fix

--- src/state.cpp
+++ src/state.cpp
@@ -1,6 +1,5 @@
 #include <state.h>
 
-////// Implementation of State methods begins //////
 State::State() {
 	state_name = "";
 }
@@ -23,24 +22,17 @@ States State::operator+(const State & rhs) {
 	ss.add(*this).add(rhs);
 	return ss;
 }
-////// Implementation of State methods ends //////
 
-<<<<<<< HEAD
-
-
-////// Implementation of States methods begins //////
-=======
-States State::operator+(States ss){
+States State::operator+(States ss) {
 	ss.add(*this);
 	return ss;
 }
 
->>>>>>> ca3797b0c83468809636afbf78f38f73d9444f47
 States::States(std::vector<State> &_state_vector) {
 	state_vector = _state_vector;
 }
 
-//The below is similar to the overloaded + operator
+//The below is similar to the overloaded += operator
 States & States::add(const State &s) {
 	state_vector.push_back(s);
 	return (*this);
@@ -64,13 +56,9 @@ void States::set(int index, const State &s) {
 	state_vector[index] = s;	
 }
 
-<<<<<<< HEAD
 //The below adds the state on the right hand side to the
 //States object on the left hand side
-States & States::operator+(const State & rhs) {
-=======
 States & States::operator+=(const State & rhs) {
->>>>>>> ca3797b0c83468809636afbf78f38f73d9444f47
 	this->add(rhs);
 	return (*this);
 }
@@ -86,7 +74,6 @@ void States::print() {
 		std::cout << state_vector[i] << " ,"[i < state_vector.size() - 1] << " \n"[i == state_vector.size() - 1];
 	}
 }
-////// Implementation of States methods ends //////
 
 //overloaded insertion operator to print States
 std::ostream & operator<<(std::ostream & os, const State & rhs) {
