03aab88b1291919e7a0947cddba4df449ae7b7dd
karlhajal <karlhajal@gmail.com>
2018-02-23 16:36:13 +0000

    Added documentation for transition.h and transition.cpp

--- include/transition.h
+++ include/transition.h
@@ -7,55 +7,59 @@
 #include <port.h>
 #include <vector>
 
-class Transitions; //Class Prototype
+class Transitions; //! Class Prototype
 
 class Transition {
 private:
-	State start;
-	State destination;
-	Port port;
-	Condition condition;
-	Action action;
+	State start; //! Starting state 
+	State destination; //! Destination state
+	Port port; //! Port whose boolean value decides the validity of the transition
+	Condition condition; //! Condition which should be met for the transition to be valid
+	Action action; //! Action taken when the transition is executed
 
 public:
-	Transition(){};
-	Transition(State _start, State _destination, Port _port, Condition _condition, Action _action);
+	Transition(){};	//! Default constructor
+	Transition(State _start, State _destination, Port _port, Condition _condition, Action _action);	//! Parametrized Constructor
 
-	State getStart();
-	State getDestination();
-	Port getPort();
-	Condition getCondition();
-	Action getAction();
+	State getStart();	//! Returns the starting state
+	State getDestination();	//! Returns the destination state
+	Port getPort();	//! Returns the port
+	Condition getCondition();	//! Returns the Condition
+	Action getAction();	//! Returns the Action
         
-	void setStart(State _start);
-	void setDestination(State _destination);
-	void setPort(Port _port);
-	void setCondition(Condition _condition);
-	void setAction(Action _action);
+	void setStart(State _start);	//! Sets the starting state
+	void setDestination(State _destination);	//! Sets the destination state
+	void setPort(Port _port);	//! Sets the port
+	void setCondition(Condition _condition);	//! Sets the Condition
+	void setAction(Action _action);	//! Sets the Action
 
-	bool evaluate_condition();
-	void execute_action();
-	bool valid();
+	bool evaluate_condition();	//! Evaluates the condition and returns the result
+	void execute_action();	//! Executes the action when the transition is taken
+	bool valid();	//! Returns true if both the port is activated and the condition is met
 
-	Transitions operator+(const Transition & rhs);
-	Transitions operator+(Transitions ts);
+	Transitions operator+(const Transition & rhs);  //! Overloads the + operator to add two
+													//! transitions to a new "Transitions" object
+	Transitions operator+(Transitions ts);	//! Overloads the + operator to append a Transition to a "Transitions" object
 };
 
 class Transitions {
 private:
-	std::vector<Transition> transition_vector;
+	std::vector<Transition> transition_vector; //! Stores multiple "Transition" objects
 public:
-	Transitions(){}
-	Transitions(std::vector<Transition> _transition_vector);
-	size_t size() const;
-	std::vector<Transition> get();
-	Transition get(int index) const;
-	Transitions & add(const Transition &t);
-	Transitions & operator+=(const Transition &t);
-	void set(int index, const Transition &s);
-	Transitions operator+(const Transition &t);
-	void remove();
-	void remove(int index);
+	Transitions(){}	//! Default Constructor
+	Transitions(std::vector<Transition> _transition_vector);	//! Parametrized Constructor
+	size_t size() const;	//! Returns the size of the vector, i.e. the number of transitions
+	std::vector<Transition> get();	//! Returns transition_vector
+	Transition get(int index) const;	//! Returns the transition at a given index of the vector
+	Transitions & add(const Transition &t);	//! Appends a Transition object to the vector
+	Transitions & operator+=(const Transition &t);	//! Overloads the += operator to append
+													//! a Transition object to the vector
+	Transitions operator+(const Transition &t); //! Overloads the + operator to return a new
+												//! Transitions object consisting of the one on
+												//! the LHS in addition to the Transition on the RHS
+	void set(int index, const Transition &s);	//! Sets the element at a given index of the vector
+	void remove(); //! Removes the last element of the vector
+	void remove(int index); //! Remove the element at a given index from the vector
 };
 
 #endif
--- src/transition.cpp
+++ src/transition.cpp
@@ -1,5 +1,15 @@
 #include <transition.h>
 
+
+////////////////////////
+/// Transition Class ///
+////////////////////////
+
+
+////
+/// Parametrized Constructor
+/// Sets start, destination, port, condition and action
+////
 Transition::Transition(State _start, State _destination, Port _port, Condition _condition, Action _action) {
 	start = _start;
 	destination = _destination;
@@ -8,6 +18,8 @@ Transition::Transition(State _start, State _destination, Port _port, Condition _
 	action = _action;
 }
 
+////
+/// Getters
 State Transition::getStart() {
 	return start;
 }
@@ -27,7 +39,10 @@ Condition Transition::getCondition() {
 Action Transition::getAction() {
 	return action;
 }
+////
 
+////
+/// Setters
 void Transition::setStart(State _start) {
 	start = _start;
 }
@@ -47,72 +62,126 @@ void Transition::setCondition(Condition _condition) {
 void Transition::setAction(Action _action) {
 	action = _action;
 }
+////
 
+////
+///	Returns whether the condition is met or not
+////
 bool Transition::evaluate_condition() {
 	return condition.evaluate();
 }
 
+////
+/// Executes the action when the transition is taken
+////
 void Transition::execute_action() {
 	action.execute();
 }
 
+////
+/// Returns whether both the port is activated 
+/// and the condition is met
+////
 bool Transition::valid() {
 	return port.status() && condition.evaluate();
 }
 
+////
+/// Overloads the + operator to add two
+/// transitions to a new "Transitions" object
+////
 Transitions Transition::operator+(const Transition & rhs) {
 	Transitions t;
 	t.add(*this).add(rhs);
 	return t;
 }
-
+////
+/// Overloads the + operator to append
+/// a transition to a "Transtions" object
+////
 Transitions Transition::operator+(Transitions ts) {
 	ts.add(*this);
 	return ts;
 }
 
+/////////////////////////
+/// Transitions Class ///
+/////////////////////////
 
+////
+/// Parametrized Constructor
+////
 Transitions::Transitions(std::vector<Transition> _transition_vector) {
 	transition_vector = _transition_vector;
 }
 
+////
+/// Returns the size of the vector, i.e. the number of transitions
+////
+size_t Transitions::size() const {
+	return transition_vector.size();
+}
+
+////
+/// Returns the vector of transitions
+////
 std::vector<Transition> Transitions::get() {
 	return transition_vector;
 }
 
+////
+/// Returns the transition at
+/// a given index of the vector
+////
 Transition Transitions::get(int index) const {
 	return transition_vector.at(index);
 }
 
+////
+/// Appends a Transition object to the vector
+////
 Transitions & Transitions::add(const Transition &t) {
 	transition_vector.emplace_back(t);
 	return (*this);
 }
 
+////
+/// Overloads the += operator to append
+/// a Transition object to the vector
+////
+Transitions & Transitions::operator+=(const Transition &t) {
+	transition_vector.emplace_back(t);
+	return (*this);
+}
+
+////
+/// Overloads the + operator to return a new
+/// Transitions object consisting of the one on
+/// the LHS in addition to the Transition on the RHS
+////
 Transitions Transitions::operator+(const Transition &t) {
 	Transitions ts = *this;	
 	ts.add(t);
 	return ts;
 }
 
-Transitions & Transitions::operator+=(const Transition &t) {
-	transition_vector.emplace_back(t);
-	return (*this);
-}
-
+////
+/// Sets the element at a given index of the vector
+////
 void Transitions::set(int index, const Transition &s) {
-	//check index
-	transition_vector[index] = s;
+	transition_vector.at(index) = s;
 }
 
-size_t Transitions::size() const {
-	return transition_vector.size();
-}
-
+////
+/// Removes the last element of the vector
+////
 void Transitions::remove() {
 	transition_vector.pop_back();
 }
 
+////
+/// Remove the element at a given index from the vector
+////
 void Transitions::remove(int index) {
 	transition_vector.erase(transition_vector.begin()+index);
 }
